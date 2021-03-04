f4ac123b41f8e0bd4139df46d52eb0f1a8fcdb4d
jarjanazy <celil.cercenazi@gmail.com>
2021-03-04 20:20:44 +0300

    use JSX to create the Car component

--- Raw JS/demo/src/Car.js
+++ Raw JS/demo/src/Car.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default function Car ({ name, prodYear, price }) {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, name),
-    React.createElement("h2", {}, prodYear),
-    React.createElement("h2", {}, price),
-  ]);
+  //return React.createElement("div", {}, [
+  //  React.createElement("h2", {}, name),
+  //  React.createElement("h2", {}, prodYear),
+  //  React.createElement("h2", {}, price),
+  //]);
+
+  // this code translates to the one on top
+  // this is far more readable that's why we use it
+  return (
+    <div> 
+      <h2>{name}</h2>
+      <h2>{prodYear}</h2>
+      <h2>{price}</h2>
+    </div>
+  )
+
+
 };
