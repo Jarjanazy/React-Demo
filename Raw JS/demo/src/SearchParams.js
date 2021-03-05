6d1086b2593ca38afcbc43fdbf52e0224942d84a
jarjanazy <celil.cercenazi@gmail.com>
2021-03-05 14:31:37 +0300

    Make the searchParams component respond to DOM events (update the input field)

--- Raw JS/demo/src/SearchParams.js
+++ Raw JS/demo/src/SearchParams.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const searchParams = () => {
-    const location = 'Seattle, WA';
+    const [location, setLocation] = useState('Seattle, WA');
 
     return (
-        // we use className instead of class, because in JS class is a reserved word
-        // the same goes for htmlFor
+        // when we try to edit the input field we notice that it doesn't change
+        // it's because on every event on the DOM React kicks off a re-render cycle on its components
+        // and on reload the value of the input field is Seattle, WA when the App componenet is re-rendered
         <div className="search-params"> 
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="location" value={location} placeholder="Location"/>
+                    <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
                     <button> Submit</button>
                 </label>
             </form>
