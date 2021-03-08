8f5f5815f3be629a8ed4225eb54e7b7515bf2a4d
jarjanazy <celil.cercenazi@gmail.com>
2021-03-08 21:21:38 +0300

    mock an API call and display data on a list

--- Raw JS/demo/src/SearchParams.js
+++ Raw JS/demo/src/SearchParams.js
@@ -1,19 +1,33 @@
 import React, {useState} from 'react';
 
+const APIMock = ['Dog', 'Cat', 'Bird']
+
 const searchParams = () => {
-    const [location, setLocation] = useState('Seattle, WA');
+    const [location, setLocation] = useState('Seattle, WA'); // set default as Seattle, WA
+    const [animal, setAnimal] = useState('dog');
 
     return (
         // when we try to edit the input field we notice that it doesn't change
         // it's because on every event on the DOM React kicks off a re-render cycle on its components
         // and on reload the value of the input field is Seattle, WA when the App componenet is re-rendered
         <div className="search-params"> 
+            <h1>This is also changed {location} </h1>
             <form>
                 <label htmlFor="location">
                     Location
                     <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)}/>
                     <button> Submit</button>
                 </label>
+                <label htmlFor='animal'>
+                    Animal
+                    <select id="animal" 
+                    value={animal} 
+                    onChange={e => setAnimal(e.target.value)}
+                    onBlur={e => setAnimal(e.target.value)}>
+                        {APIMock.map(animal => <option value={animal}>{animal}</option>)}
+                    </select>
+                </label>
+
             </form>
         </div>
 
