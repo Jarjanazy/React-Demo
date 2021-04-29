632f3d14d48e9d9fa87110cd9f76cc175aeaacff
jarjanazy <celil.cercenazi@gmail.com>
2021-04-29 22:21:09 +0300

    Add initial code for MovieList, Movie components

---
+++ lord of the rings/lord-of-the-rings/src/domain/MovieList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Movie from './Movie'
+
+const MovieList = () => {
+    return (
+        <div>
+            <Movie/>
+            <Movie/>
+        </div>
+    );
+}
+
+export default MovieList;
