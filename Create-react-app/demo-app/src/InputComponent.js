a8df3b8dd9be2f95482b8fbef492052eecdcb472
jarjanazy <celil.cercenazi@gmail.com>
2021-04-29 15:41:46 +0300

    Add the initial code for the lord of the rings project

--- Create-react-app/demo-app/src/InputComponent.js
+++ Create-react-app/demo-app/src/InputComponent.js
@@ -1,16 +1,30 @@
 import React, {useState} from 'react'
 
-
-
 const InputComponent = () =>{
-  const [input, setInput] = useState('Hello world');
-  const setTheValueToInput = () => setInput(document.getElementById("input").value);
+  const [input, setInput] = useState({
+    "key" : "hello key",
+    "value" : "hello value"
+  });
+
+  const setTheValueToKey = () => {
+    const key = document.getElementById("key").value;
+    setInput({...input, "key" : key})
+  };
+  const setTheValueToValue = () => {
+    const value = document.getElementById("value").value;
+    setInput({...input, "value" : value});
+  };
 
     return (
     <article>
-      <h3>{input}</h3>  
-      <button onClick={setTheValueToInput}>Change the value</button>
-      <textarea id="input"></textarea>
+      <h3>key is {input.key}</h3>
+      <h3>value is {input.value}</h3>  
+
+      <button onClick={setTheValueToKey}>Change the key</button>
+      <button onClick={setTheValueToValue}>Change the value</button>
+
+      <textarea id="key"></textarea>
+      <textarea id="value"></textarea>
     </article>
     );
   }
