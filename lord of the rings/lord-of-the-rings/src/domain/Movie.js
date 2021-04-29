598bd5b043c179dccea9ecb9ffe33a878472bd2f
jarjanazy <celil.cercenazi@gmail.com>
2021-04-29 23:07:32 +0300

    Add bootstrap to the index.html file

--- lord of the rings/lord-of-the-rings/src/domain/Movie.js
+++ lord of the rings/lord-of-the-rings/src/domain/Movie.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Movie = () => {
+const Movie = ({attr}) => {
     return (
-        <div>
-            <h2> MOVIE </h2>
+        <div className="card">
+            <h2> {attr.name} </h2>
+            <h2> {attr.minuteLength} </h2>
+            <h2> {attr.budgetInMillion} </h2>
+            <h2> {attr.revenueInMillion} </h2>
+            <h2> {attr.awardCount} </h2>
         </div>
     );
 }
--- lord of the rings/lord-of-the-rings/src/domain/MovieList.js
+++ lord of the rings/lord-of-the-rings/src/domain/MovieList.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Movie from './Movie'
 
+
+const firstMovie = {
+    awardCount : 2,
+    revenueInMillion : 3,
+    budgetInMillion : 4,
+    minuteLength : 5,
+    name : "test name"
+}
+
 const MovieList = () => {
     return (
         <div>
-            <Movie/>
-            <Movie/>
+            <Movie attr={firstMovie}/>
         </div>
     );
 }
