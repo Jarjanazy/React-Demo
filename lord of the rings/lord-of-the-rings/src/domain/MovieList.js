9ccfa9b0d93799729731cfa83dfd9f4bbadbe26e
jarjanazy <celil.cercenazi@gmail.com>
2021-04-30 22:48:08 +0300

    Make lord of the rings work using useEffect hook

--- lord of the rings/lord-of-the-rings/src/domain/MovieList.js
+++ lord of the rings/lord-of-the-rings/src/domain/MovieList.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Movie from './Movie'
 
 
-const firstMovie = {
-    awardCount : 2,
-    revenueInMillion : 3,
-    budgetInMillion : 4,
-    minuteLength : 5,
-    name : "test name"
-}
-
-
-
-const getMoviesFromAPI = (callbackFun) => {
-    fetch('https://the-one-api.dev/v2/movie', {
-        headers: {
-            'Authorization': 'Bearer b8pAHjjXVh68hZO83wlq',
-          }
-    })
-    .then(res => res.json())
-    .then((data) => {
-        console.log("HEEEEEY");
-        callbackFun([data.docs[0]])
-    })
-    .catch(console.log)
-}
-
 const MovieList = () => {
-    const [movies, setMovies] = useState([firstMovie]);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch('https://the-one-api.dev/v2/movie', {
+            headers: {
+                'Authorization': 'Bearer b8pAHjjXVh68hZO83wlq',
+              }
+        })
+        .then(res => res.json())
+        .then((data) => {setMovies(data.docs)})
+        .catch(console.log)
+    });
 
     return (
         <div>
@@ -38,8 +25,6 @@ const MovieList = () => {
                     </div>
                     )
                 })}
-
-                <button onClick={() => getMoviesFromAPI(setMovies)}> refresh</button>
         </div>
     );
 }
