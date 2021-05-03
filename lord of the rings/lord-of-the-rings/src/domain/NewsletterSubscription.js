76f2a446788d9b5b63d60e66d2d470be80e9a346
jarjanazy <celil.cercenazi@gmail.com>
2021-05-04 00:58:00 +0300

    Add a submit event handler

--- lord of the rings/lord-of-the-rings/src/domain/NewsletterSubscription.js
+++ lord of the rings/lord-of-the-rings/src/domain/NewsletterSubscription.js
@@ -1,11 +1,16 @@
 import FormElement from "./FormElement"
 
+const doSubmit = (e) => {
+    // by default the browser tries to submit the form and refresh the page
+    e.preventDefault();
+    console.log("Hello world");
+}
 const NewsLetterSubscription = () => {
-
     return (
-        <form className="form">
+        <form className="form" onSubmit={doSubmit}>
             <FormElement id={"name"} name={"name"} text={"Name:"}/>
             <FormElement id={"email"} name={"email"} text={"Email:"}/>
+            <button type="submit"> Subscribe </button>
         </form>
     );
 }
