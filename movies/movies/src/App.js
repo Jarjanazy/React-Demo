06be817b1111447f23681601ae0888e07fba3a41
jarjanazy <celil.cercenazi@gmail.com>
2021-05-04 17:09:12 +0300

    Add the initial code to call the API

--- movies/movies/src/App.js
+++ movies/movies/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://www.omdbapi.com/?apikey=4926c71a&i=tt3896198', {})
+        .then(res => res.json())
+        .then((data) => {console.log(data)})
+        .catch(console.log)
+  });
   return (
     <h2> HELLO </h2>
   );
