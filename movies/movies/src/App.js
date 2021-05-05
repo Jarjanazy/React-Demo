a9fb3e5ab375419cebf84d7a09ae951492e49883
jarjanazy <celil.cercenazi@gmail.com>
2021-05-05 21:15:28 +0300

    Add the movie name as a header

--- movies/movies/src/App.js
+++ movies/movies/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
 import {useState} from 'react';
 
-
 import SearchBar from './application/SearchBar';
+import Movie from './domain/Movie';
+
+const objectIsEmpty = (obj) => {
+  return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
+const setApiResponseToMovie = (response, setMovie) => {
+  console.log(response);
+
+  if (response.Response === "False"){
+    setMovie({"Title" : "No Such Movie"});
+  }
+  else{
+    setMovie(response);
+  }
+}
 
 function App() {
   const [keyword, setKeyword] = useState("");
+  const [movie, setMovie] = useState({});
 
  const callAPI = () => {
     const url = `http://www.omdbapi.com/?apikey=4926c71a&t=${keyword}`;
     fetch(url, {})
         .then(res => res.json())
-        .then((data) => {console.log(data)})
+        .then((data) => {setApiResponseToMovie(data, setMovie)})
         .catch(console.log)
   };
 
@@ -19,6 +35,7 @@ function App() {
     <div>
       <button type="submit" onClick={callAPI}>Search</button>
       <SearchBar keyword={keyword} setKeyword={setKeyword}/>
+      {!objectIsEmpty(movie) && <Movie attr={movie}/>}
     </div>
   );
 
---
+++ movies/movies/src/domain/Movie.js
@@ -0,0 +1,10 @@
+const Movie = ({attr}) =>{
+    return (
+        <>
+        <h2>{attr.Title}</h2>
+        
+        </>
+    );
+};
+
+export default Movie;
