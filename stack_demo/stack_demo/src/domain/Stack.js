56ad6cdf36d989b1889896ea76b5189796ef4f9a
jarjanazy <celil.cercenazi@gmail.com>
2021-05-12 23:29:44 +0300

    Add initial chakra-UI elements

---
+++ stack_demo/stack_demo/src/domain/Stack.js
@@ -0,0 +1,15 @@
+import {VStack, Text} from '@chakra-ui/react';
+
+const Stack = () => {
+
+    return (
+    <VStack>
+        <Text>HEY 1</Text>
+        <Text>HEY 2</Text>
+        <Text>HEY 3</Text>
+    </VStack>
+    
+    );
+}
+
+export default Stack;
