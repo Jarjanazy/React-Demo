814b752ed94c799f9a71ebb639f85f5afe470aab
jarjanazy <celil.cercenazi@gmail.com>
2021-05-13 21:40:04 +0300

    add a remove last block button to the stack project

--- stack_demo/stack_demo/src/App.js
+++ stack_demo/stack_demo/src/App.js
@@ -4,14 +4,35 @@ import { VStack, HStack, Button} from '@chakra-ui/react'
 import {useReducer} from 'react'
 
 const ACTIONS = {
-  ADD_BLOCK : "add_block"
+  ADD_BLOCK : "add_block",
+  REMOVE_LAST_BLOCK : "remove_last_block"
 }
 
-const reduce = (oldState, action) => {
-  if (action.type === ACTIONS.ADD_BLOCK){
-    return {blockCount : oldState.blockCount + 1};
+const reduce = (currentState, action) => {
+  if (action.type === ACTIONS.ADD_BLOCK)
+    return {blockCount : currentState.blockCount + 1};
+  
+  else if (action.type === ACTIONS.REMOVE_LAST_BLOCK){
+    if (currentState.blockCount === 0) 
+      return currentState;
+    else 
+      return {blockCount : currentState.blockCount - 1};
   }
+  
   else throw new Error();
+}
+
+// whenever we call the dispatch function with an object
+// the reduce function is called with the first parameter as our current state
+// and the second parameter as teh object we gave to the dispatch function
+// the reduce function will return the new updated state
+
+const addNewBlock = (dispatch) => {
+  dispatch({type: ACTIONS.ADD_BLOCK});
+}
+
+const removeLastBlock = (dispatch, payload) => {
+  dispatch({type : ACTIONS.REMOVE_LAST_BLOCK});
 }
 
 function App() {
@@ -20,10 +41,9 @@ function App() {
   return (
   <VStack>
     <HStack spacing="50px">
+    <   Button colorScheme="red" size="lg" onClick={() => removeLastBlock(dispatch)}> Remove last block </Button>
         <Stack state={state}/>
-        <Button colorScheme="teal" size="lg" 
-          onClick={() => dispatch({type: ACTIONS.ADD_BLOCK})}> Add new block 
-        </Button>
+        <Button colorScheme="teal" size="lg" onClick={() => addNewBlock(dispatch)}> Add new block </Button>
     </HStack>
   </VStack>
   );
